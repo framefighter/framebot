3a885cc706f36c86f6de79a34113fcb8e924b01d
framefighter <leo@mainfeld.de>
2019-09-03 19:55:49 +0200

    fixed script

--- bash/restart.sh
+++ bash/restart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Pulling new git version"
 git pull -q
 echo "Installing possible new npm modules"
@@ -14,4 +13,4 @@ echo "This may take a few seconds"
 echo "Killing sessions"
 
 tmux ls
-tmux new-session -d -s restart "bash bash/kill.sh; bash bash/start.sh"
+tmux new-session -d -s restart "tmux kill-session -t teleframe; tmux new -d -s teleframe 'npm start'"
