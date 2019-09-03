d1425de75d921bbba258028b2f953d17210b70b7
framefighter <leo@mainfeld.de>
2019-09-03 19:51:46 +0200

    only set -e on typescr ipt error

--- bash/restart.sh
+++ bash/restart.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
 echo "Pulling new git version"
 git pull -q
 echo "Installing possible new npm modules"
 npm i -qe
 echo "Compiling new typescript build"
-tsc -e
+set -e
+tsc
+set +e
 echo "Preparing to restart bot"
 echo "This may take a few seconds"
 echo "Killing sessions"
+
 tmux ls
 tmux new-session -d -s restart "bash bash/kill.sh; bash bash/start.sh"
