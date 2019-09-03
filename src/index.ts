97c89c97ca8984ef406e00165fc27aff2b6ea91a
framefighter <leo@mainfeld.de>
2019-09-03 16:42:53 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { Bot } from './bot/bot';
 import { Commands } from './bot/command/commands';
-
 const commands = new Commands();
-
 export const BOT = new Bot({ commands });
 export * from './bot/command/definitions';
