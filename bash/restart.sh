bffad444162e4304b94d8368800f26f7605fafbe
framefighter <leo@mainfeld.de>
2019-09-04 22:07:10 +0200

    updated packages

--- bash/restart.sh
+++ bash/restart.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 echo "Pulling new git version"
 git pull -q
 echo "Installing possible new npm modules"
-npm i -qe
+npm i
 echo "Updating modules"
 npm update
 echo "Compiling new typescript build"
@@ -13,7 +12,5 @@ set +e
 echo "Preparing to restart bot"
 echo "This may take a few seconds"
 echo "Killing sessions"
-
 tmux ls
 tmux new-session -d -s restart "tmux kill-session -t teleframe; tmux new -d -s teleframe 'npm start'"
-
