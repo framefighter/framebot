7010bae83621ba284fb3cbb5fa6b01f7d0d69fda
framefighter <leo@mainfeld.de>
2019-09-05 15:28:41 +0200

    added stat to pull

--- bash/restart.sh
+++ bash/restart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo "Pulling new git version"
-git pull -q
+git pull -n
+git show --shortstat
+# BRAK ON ERROR
+set -e
 echo "Installing possible new npm modules"
 npm i
 echo "Updating modules"
 npm update
 echo "Compiling new typescript build"
-set -e
 tsc
 set +e
+# DONT BREAK ON ERROR
 echo "Preparing to restart bot"
-echo "This may take a few seconds"
 echo "Killing sessions"
 tmux ls
 tmux new-session -d -s restart "tmux kill-session -t teleframe; tmux new -d -s teleframe 'npm start'"
